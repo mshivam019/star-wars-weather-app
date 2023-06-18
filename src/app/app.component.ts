f49884a3ee91e4f1eb4e8e02fd39f8f16b8e66f0
mshivam019 <mshivam019@gmail.com>
2023-06-18 23:49:41 +0530

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,33 +1,138 @@
 import { Component, OnInit } from '@angular/core';
 import { weatherData } from './models/weather.models';
 import { WeatherService } from './services/weather.service';
+import { planetData } from './planet-data';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   title = 'weather-app';
-  constructor(private weatherService:WeatherService){
-    
-  }
-  cityName: string='Jamshedpur';
-  weatherData?:weatherData
-  ngOnInit():void{
+  cityName: string = 'Jamshedpur';
+  weatherData?: weatherData;
+  planetName: string = '';
+  planetDescription: string = '';
+  planetHiddenMessage: string = '';
+  planetImageSrc: string = '';
+
+  constructor(private weatherService: WeatherService) {}
+
+  ngOnInit(): void {
     this.getWeatherData(this.cityName);
-    this.cityName='';
+    this.cityName = '';
   }
-  onSubmit(){
+
+  onSubmit(): void {
     this.getWeatherData(this.cityName);
-    this.cityName='';
+    this.cityName = '';
   }
-  private getWeatherData(cityName:string) {
+
+  private getWeatherData(cityName: string): void {
     this.weatherService.getWeatherData(cityName).subscribe({
-      next: (response) =>{
-        this.weatherData=response;
+      next: (response) => {
+        this.weatherData = response;
+        this.updatePlanetDetails();
         console.log(response);
+      },
+      error: (error) => {
+        this.weatherData = undefined; 
+        this.updatePlanetDetails();
+        console.log(error);
       }
-    }); 
+    });
+  }
+  getWeatherMessage(): string {
+    if (!this.weatherData) {
+      return 'Huh?';
+    }
+  
+    const temperature = this.weatherData.main?.temp;
+    const description = this.weatherData.weather?.[0]?.description;
+  
+    if (temperature && description) {
+      if (temperature < 10 && description.includes('rain')) {
+        return 'Yikes! It is cold and rainy.';
+      } else if (temperature > 30 && description.includes('clear')) {
+        return 'Whew! It is hot and sunny.';
+      } else if (description.includes('cloud')) {
+        return 'Ahh, it is cloudy.';
+      } else if (description.includes('snow')) {
+        return 'Oh my, it is snowing.';
+      } else if (description.includes('thunderstorm')) {
+        return 'Ugh, there is a thunderstorm.';
+      } else if (description.includes('fog')) {
+        return 'Wow, it is foggy.';
+      } else if (description.includes('drizzle')) {
+        return 'Oh, it is drizzling.';
+      }
+    }
+  
+    return 'Hmm'; 
+  }
+  
+
+  updatePlanetDetails(): void {
+    if (!this.weatherData || !this.weatherData.name) {
+      this.planetName = 'Alderaan';
+      this.planetDescription = 'Try again, or try another city.';
+      this.planetHiddenMessage =
+        "It might be a temporary error, or the system might not know that place.";
+      this.planetImageSrc = 'assets/images/alderaan.png';
+      return;
+    }
+
+    const cityName = this.weatherData.name.toLowerCase();
+    let planet = planetData.find(
+      (p) => p.name.toLowerCase() === cityName
+    );
+
+    if (!planet) {
+      planet = planetData.find(
+        (p) => p.name.toLowerCase() === 'alderaan'
+      );
+    }
+
+    if (!planet) {
+      return;
+    }
+
+    this.planetName = planet.name;
+    this.planetDescription = planet.description;
+    this.planetHiddenMessage = planet.hiddenMessage;
+    this.planetImageSrc = planet.imageSrc;
+
+    const filteredTemp = Math.round(this.weatherData.main.temp * 9 / 5 + 32);
+
+    if (filteredTemp <= 35) {
+      const hothPlanet = planetData.find((p) => p.name.toLowerCase() === 'hoth');
+      if (hothPlanet) this.updatePlanetData(hothPlanet);
+    } else if (filteredTemp <= 55) {
+      const nabooPlanet = planetData.find((p) => p.name.toLowerCase() === 'naboo');
+      if (nabooPlanet) this.updatePlanetData(nabooPlanet);
+    } else if (filteredTemp <= 65) {
+      const kaminoPlanet = planetData.find((p) => p.name.toLowerCase() === 'kamino');
+      if (kaminoPlanet) this.updatePlanetData(kaminoPlanet);
+    } else if (filteredTemp <= 72) {
+      const dagobahPlanet = planetData.find((p) => p.name.toLowerCase() === 'dagobah');
+      if (dagobahPlanet) this.updatePlanetData(dagobahPlanet);
+    } else if (filteredTemp <= 78) {
+      const tatooinePlanet = planetData.find((p) => p.name.toLowerCase() === 'tatooine');
+      if (tatooinePlanet) this.updatePlanetData(tatooinePlanet);
+    } else if (filteredTemp <= 90) {
+      const bespinPlanet = planetData.find((p) => p.name.toLowerCase() === 'bespin');
+      if (bespinPlanet) this.updatePlanetData(bespinPlanet);
+    } else {
+      const yavin4Planet = planetData.find((p) => p.name.toLowerCase() === 'yavin 4');
+      if (yavin4Planet) this.updatePlanetData(yavin4Planet);
+    }
+  }
+
+  updatePlanetData(planet: any): void {
+    this.planetName = planet.name;
+    this.planetDescription = planet.description;
+    this.planetHiddenMessage = planet.hiddenMessage;
+    this.planetImageSrc = planet.imageSrc;
   }
 }
---
+++ src/app/planet-data.ts
@@ -0,0 +1,59 @@
+
+
+export const planetData = [
+    {
+      name: "Kamino",
+      description: "Wet.",
+      hiddenMessage: "There's also a significant chance of unconvincing CGI aliens.",
+      imageSrc: "assets/images/kamino.png"
+    },
+    {
+      name: "Endor",
+      description: "Temperate, but grey and cloudy",
+      hiddenMessage: "Stick around and you may get your own animated series.",
+      imageSrc: "assets/images/endor.jpg"
+    },
+    {
+      name: "Hoth",
+      description: "Cold, Icy, Freezing Desolation.",
+      hiddenMessage: "You may have to climb inside a tauntaun for warmth.",
+      imageSrc: "assets/images/hoth.png"
+    },
+    {
+      name: "Tatooine",
+      description: "Hot, Dry, Occasional Sarlacc.",
+      hiddenMessage: "Utinni!",
+      imageSrc: "assets/images/tatooine.jpg"
+    },
+    {
+      name: "Bespin",
+      description: "Fog, mist, cloud, Can't see a thing.",
+      hiddenMessage: "Also, Lando is going to betray you.",
+      imageSrc: "assets/images/bespin.jpg"
+    },
+    {
+      name: "Yavin 4",
+      description: "Hot, but with some cloud in the sky.",
+      hiddenMessage: "Also bits of the Death Star might fall on your head.",
+      imageSrc: "assets/images/yavin4.jpg"
+    },
+    {
+      name: "Naboo",
+      description: "Temperate, dry, and fairly pleasant.",
+      hiddenMessage: "Meesa like-a the weather these days.",
+      imageSrc: "assets/images/naboo.jpg"
+    },
+    {
+      name: "Dagobah",
+      description: "Hot and wet, and not in a good way.",
+      hiddenMessage: "Also Yoda might be hiding somewhere.",
+      imageSrc: "assets/images/dagobah.jpg"
+    },
+    {
+      name: "Alderaan",
+      description: "Try again, or try another city.",
+      hiddenMessage: "It might be a temporary error, or the system might not know that place.",
+      imageSrc: "assets/images/alderaan.png"
+    }
+  ];
+  
